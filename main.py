160104c5da114846e902666b1a6c1423d1d2d026
Abdullah Abdul <84358031+AbdullahAbdulMajit@users.noreply.github.com>
2021-06-22 22:50:09 +0000

    VERSION 1: Leaderboard Window New class Quit button to leaderboard Added import messagebox
    VERSION 2: Question Window
    Font colour change
    Text box colour change

--- main.py
+++ main.py
@@ -44,19 +44,19 @@ class Startingwindow:
         self.quiz_frame.grid()
 
         #TITLE
-        self.heading_label = Label(self.quiz_frame, text = "GEOGRAPHY QUIZ", font = ("TW Cent MT", "18", "bold"), bg = background_color)
+        self.heading_label = Label(self.quiz_frame, text = "GEOGRAPHY QUIZ", fg = "#A52A2A", font = ("TW Cent MT", "18", "bold"), bg = background_color)
         self.heading_label.grid(row = 0)
 
         #USER INFO
-        self.user_label= Label(self.quiz_frame, text = "Enter your name:", font = ("TW Cent MT","16"), bg = background_color)
+        self.user_label= Label(self.quiz_frame, text = "Enter your name:", fg = "#A52A2A", font = ("TW Cent MT","16"), bg = background_color)
         self.user_label.grid(row = 1, pady = 20) 
         
         #ENTRY BOX
-        self.entry_box= Entry(self.quiz_frame)
+        self.entry_box= Entry(self.quiz_frame, background = 'Light Gray', borderwidth = 2, relief = SUNKEN)
         self.entry_box.grid(row = 2, pady = 20)
         
         #BUTTON
-        self.continue_button = Button(self.quiz_frame, text = "START", bg = "#3F9D2F", command = self.name_collection)
+        self.continue_button = Button(self.quiz_frame, text = "START", fg = "White", bg = "#3F9D2F", command = self.name_collection)
         self.continue_button.grid(row = 3, pady = 20) 
        
     def name_collection(self):
@@ -85,19 +85,19 @@ class Questionwindow:
     self.question_label.grid(row = 2, pady = (20, 20))
 
     #Selection options for each questions displayed using radio buttons.
-    self.rb1 = Radiobutton(self.quiz_frame, text = q_a[quenum][1], value = 1, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana", "11"), relief = RAISED, wraplength = 88, width = 40, bg = "Orange", activebackground = "Dark Orange")
+    self.rb1 = Radiobutton(self.quiz_frame, text = q_a[quenum][1], value = 1, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana", "11"))
     self.rb1.grid(row = 3, sticky = W, column = 0)
 
-    self.rb2 = Radiobutton(self.quiz_frame, text = q_a[quenum][2], value = 2, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana","11"), relief = RAISED, wraplength = 88, width = 40, bg = "Orange", activebackground = "Dark Orange")
+    self.rb2 = Radiobutton(self.quiz_frame, text = q_a[quenum][2], value = 2, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana","11"))
     self.rb2.grid(row = 3,  sticky = E, column = 0)
 
-    self.rb3 = Radiobutton(self.quiz_frame, text = q_a[quenum][3], value = 3, indicator = 0, variable = self.var1, command = self.test_progress,  pady = 60, font = ("Verdana","11"), relief = RAISED, wraplength = 88, width = 40, bg = "Orange", activebackground = "Dark Orange")
+    self.rb3 = Radiobutton(self.quiz_frame, text = q_a[quenum][3], value = 3, indicator = 0, variable = self.var1, command = self.test_progress,  pady = 60, font = ("Verdana","11"))
     self.rb3.grid(row = 4,sticky = W, column = 0)
 
-    self.rb4 = Radiobutton(self.quiz_frame, text = q_a[quenum][4], value = 4, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana","11"), relief = RAISED, wraplength = 88, width = 40, bg = "Orange", activebackground = "Dark Orange")
+    self.rb4 = Radiobutton(self.quiz_frame, text = q_a[quenum][4], value = 4, indicator = 0, variable = self.var1, command = self.test_progress, pady = 60, font = ("Verdana","11"))
     self.rb4.grid(row = 4,  sticky = E, column = 0)
 
-    #Quit button if user wants to quit during the game.
+    #Quit button if user wants to quit during the quiz.
     self.quit_button = Button(self.quiz_frame, text = "QUIT", padx = 2, pady =1, bg = "Red", relief = RAISED, command = self.quitButton)
     self.quit_button.grid(row = 0, sticky = W, padx = 10, pady = (10,4))
 
@@ -105,12 +105,12 @@ class Questionwindow:
     self.score_update = Label(self.quiz_frame, text = "Score: " + str(score), bg = background_color, font = ("bold"))
     self.score_update.grid(row = 0, padx = 20, pady = (10,4), sticky = E)
         
-  #If user wants to leave during the game in progress, user is asked to confirm, then taken to leaderboard.
+  #If user wants to leave during the quiz in progress, user is asked to confirm, then taken to leaderboard.
   def quitButton(self):
-    result = messagebox.askyesno("Exit Game", "Are you sure?") #Ask user to confirm
+    result = messagebox.askyesno("Exit", "Are you sure?") #Ask user to confirm
     if result == True: #if yes, then takes user to leaderboard.
       self.leader_board()
-    else: #or else continue game.
+    else: #or else continue quiz.
       pass 
 
   def radioEvent(self): #Change answers for each question after user selects answer.
@@ -127,8 +127,8 @@ class Questionwindow:
     global score
     score_label = self.score_update
     selected = self.var1.get()
-    if len(asked) > 6: #at the end of the quiz: 
-      if selected == q_a[quenum][6]: #user is taken to the leaderboard to show their results. 
+    if len(asked) > 4: #at the end of the quiz: 
+      if selected == q_a[quenum][4]: #user is taken to the leaderboard to show their results. 
         score += 10
         score_label.config(text = "Score: " + str(score))
         self.leader_board()
@@ -149,40 +149,43 @@ class Questionwindow:
         score_label.config(text = "Oops! The correct answer was " + q_a[quenum][5] + "\nScore:"  + str(score))
         self.radioEvent()
     
-    #Initialising the leaderboard to list top 20 names with the highest scores so far from the stored_usernames.
-    def leader_board(self):
-        root.withdraw()
-        name = stored_usernames[0]
-        file = open("leaderBoard.txt", "a") #insert the leader file in here.
+  #Initialising the leaderboard to list top 5 names with the highest scores so far from the stored_usernames.
+  def leader_board(self):
+    root.withdraw()
+    name = stored_usernames[0]
+    file = open("leaderBoard.txt", "a") #insert the leader file in here.
 
-        if name == "admin":
-            file = open("leaderBoard.txt", "w")
-        else:
-            file.write(str(score)) #mention score along with user name inputted by the user.
-            file.write(" - ")
-            file.write(name + "\n") 
-            file.close()
+    if name == "admin":
+      file = open("leaderBoard.txt", "w")
+    else:
+      file.write(str(score)) 
+      file.write(" - ")
+      file.write(name + "\n") #mention score along with user name inputted by the user.
+      file.close()
         
-        inputFile = open("leaderBoard.txt", 'r')
-        lineList = inputFile.readlines()
-        lineList.sort()
+    inputFile = open("leaderBoard.txt", 'r')
+    lineList = inputFile.readlines()
+    lineList.sort()
         
-        highest = [] #highest scores saved in this list
-        highest_20 = (lineList[-20:])#top 20 highest scores will e displayed.
-        for line in highest_20:
-            point = line.split(" - ") #format each line.
-            highest.append((int(point[0]), point[1]))
-        file.close()
-        highest.sort()
-        highest.reverse()
-        return_string = ""
-        for i in range(len(highest)):
-            return_string += "{} - {}\n".format(highest[i][0], highest[[i][0]])
-        print(return_string)
+    inputFile = open("leaderBoard.txt", "r")
+    lineList = inputFile.readlines()
+    lineList.sort()
+    top = []
+    top5 = (lineList[-5:])
+    for line in top5:
+      point = line.split(" - ")
+      top.append((int(point[0]), point[1]))
+    file.close()
+    top.sort()
+    top.reverse()
+    return_string = ""
+    for i in range(len(top)):
+      return_string += "{} - {}\n".format(top[i][0], top[i][1])
+    print(return_string)
 
-        #Display Leaderboard in new seperate window.
-        open_leaderb = Leaderboard(root)
-        open_leaderb.num1.config(text = return_string)
+    #Display Leaderboard in new seperate window.
+    open_leaderb = Leaderboardwindow(root)
+    open_leaderb.num1.config(text = return_string)
 
 
 
@@ -193,7 +196,42 @@ class Questionwindow:
 
 
 
+##Display Leaderboard in this class:
+class Leaderboardwindow:
 
+  #Format using a Toplevel widget for a new window.
+  def __init__(self, parent):
+    background_color = "#DEBB74"
+    self.endbox = Toplevel(root)
+    self.endbox.title("Quiz Leaderboard")
+    self.endbox.geometry("600x800")
+    self.endbox.resizable(False, False)
+
+    #Leaderboard frame
+    self.leaderb_frame = Frame(self.endbox, bg = background_color, padx = 220, pady = 50)
+    self.leaderb_frame.grid()
+
+    #Congratulate user.
+    self.leaderb_heading = Label(self.leaderb_frame, text = "Leaderboard\n---", bg = background_color, fg = "Dark Red", font = ("Times New Roman", "14", "bold"))
+    self.leaderb_heading.grid(row = 0)
+
+    #Display personal score.
+    self.score_results = Label(self.leaderb_frame, text = "Your Score:           " + str(score), bg = background_color, fg = "Dark Red", font = ("13"))
+    self.score_results.grid(row = 1, pady = (0, 20))
+
+    #Display top 5 highest scores from quiz.
+    self.num1 = Label(self.leaderb_frame, text = "1st place..", bg = background_color, fg = "Dark Red", font=("Helvetica", "10"))
+    self.num1.grid(row =2)
+
+    #Quit button to exit quiz.
+    quit_program = Button(self.leaderb_frame, text = "END", command = self.endbox_destroy, padx = 10, pady = 10 ,bg = "Red")
+    quit_program.grid(row=4)
+
+
+  #End quiz when user presses end button.
+  def endbox_destroy(self):
+    self.endbox.destroy()
+    root.destroy()
 
 
 
